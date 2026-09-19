7f85938719505bdcc9d2a08f739ba0b0c7711c27
Kanika Banga <kanikabanga29@gmail.com>
2026-09-19 20:35:08 +0530

    Delete 0709-to-lower-case directory

--- 0709-to-lower-case/0709-to-lower-case.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def toLowerCase(self, s: str) -> str:
-        string = ""
-        for i in s:
-            if 65<=ord(i)<=90:
-                string += chr(ord(i)+32)
-            else:
-                string+=i
-        return string
